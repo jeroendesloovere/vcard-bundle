8c68245eaf29c506b894d8ac49a9f1249d93d6d4
Jeroen Desloovere <jeroendesloovere@gmail.com>
2015-05-29 11:36:12 +0200

    Pushed changed to DependencyInjection

---
+++ DependencyInjection/VCardExtension.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace JeroenDesloovere\VCardBundle\DependencyInjection;
+
+/*
+ * This file is part of the VCard bundle.
+ *
+ * (c) Jeroen Desloovere <info@jeroendesloovere.be>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+/**
+ * This is the class that loads and manages your bundle configuration
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
+ */
+class VCardExtension extends Extension
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+    }
+}
