2cb3df90555f7831e22faecb7ccdfd882d0153e2
Jeroen Desloovere <jeroendesloovere@gmail.com>
2015-06-01 08:20:36 +0200

    Dependency must contain namespace name

--- DependencyInjection/VCardExtension.php
+++ DependencyInjection/VCardExtension.php
@@ -22,7 +22,7 @@ use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class VCardExtension extends Extension
+class JeroenDesloovereVCardExtension extends Extension
 {
     /**
      * {@inheritDoc}
